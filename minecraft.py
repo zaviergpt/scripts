9f42216b8acac0573da3dc00967549294a93e1f0
Zavier Chiam <60832719+zaviergpt@users.noreply.github.com>
2024-06-06 13:56:51 +0800

    Update minecraft.py

--- minecraft.py
+++ minecraft.py
@@ -14,7 +14,6 @@ class Minecraft:
     def install(self):
         os.system("""
 clear
-sudo apt install openjdk-21-jdk -y
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
 sudo apt update && sudo apt upgrade -y
