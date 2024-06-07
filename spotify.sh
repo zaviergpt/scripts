11a52c1bb2c0c2eb157b53fde3232b025e336d7d
Zavier Chiam <60832719+zaviergpt@users.noreply.github.com>
2024-06-07 14:57:53 +0800

    Create spotify.sh

---
+++ spotify.sh
@@ -0,0 +1,6 @@
+sudo apt update && sudo apt upgrade
+sudo apt install -y apt-transport-https curl
+curl -sSL https://dtcooper.github.io/raspotify/key.asc | sudo tee /usr/share/keyrings/raspotify-archive-keyrings.asc >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/raspotify-archive-keyrings.asc] https://dtcooper.github.io/raspotify raspotify main' | sudo tee /etc/apt/sources.list.d/raspotify.list
+sudo apt update
+sudo apt install raspotify
