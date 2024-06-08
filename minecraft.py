4602f71ad6a446e68b76786912e463c8e6a716d9
Zavier Chiam <60832719+zaviergpt@users.noreply.github.com>
2024-06-08 21:27:29 +0800

    Update minecraft.py

--- minecraft.py
+++ minecraft.py
@@ -16,7 +16,7 @@ class Minecraft:
 clear
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
-sudo apt update && sudo apt upgrade -y
+sudo apt update
 sudo apt install playit -y
 sudo update-alternatives --auto
 sudo apt install openjdk-21-jre-headless
